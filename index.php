1d7dd99214ca03af6d157509bdc4603b6ed01a66
mfendek <mfendek@gmail.com>
2020-04-25 16:14:36 +1200

    Game data files updated to version 8.4.1

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ try {
     error_reporting(-1);
     ini_set('error_log', 'logs/oobgdw-error-' . strftime('%Y%m%d') . '.log');
 
-    $version = '2019-12-26';
+    $version = '2020-04-25';
 
     // configuration
     $dataPath = 'src/game_data/Data/';
