f26b4535525a6e91293899e40ef4db7686573ece
mfendek <mfendek@gmail.com>
2020-12-20 08:48:49 +1300

    Game data files upgraded to 8.6.5

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-const RELEASE_VERSION = '2020-09-20';
+const RELEASE_VERSION = '2020-12-20';
 const LIST_SEPARATOR = ' = ';
 const VALUE_SEPARATOR = ', ';
 const COMMANDER_ID_OFFSET = 100000;
