6cf6cce1daeba7b0f5bd3c0279f8eb9c1a82a277
mfendek <mfendek@gmail.com>
2021-10-10 07:08:14 +1300

    Game data files updated to version 9.0.6.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-const RELEASE_VERSION = '2021-02-14';
+const RELEASE_VERSION = '2021-10-10';
 const LIST_SEPARATOR = ' = ';
 const VALUE_SEPARATOR = ', ';
 const COMMANDER_ID_OFFSET = 100000;
